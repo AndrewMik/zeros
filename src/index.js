3643d40d6444811d6bcac5b11a5bad54f11c6213
AndrewMik <Andrew.Mikitchuk@gmail.com>
2018-02-24 18:40:59 +0300

    Task Zeros: Solution of the task 7

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function getZerosCount(number) {
   // your implementation
+  var devider = 5, zeroCounter = 0;
+
+  while (devider <= number){
+    zeroCounter += Math.floor(number/devider);
+    devider *= 5;
+  }
+
+  return zeroCounter;
 }
